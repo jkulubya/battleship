1a6df89deabf7eea807f944dd967108e3086761a
jkulubya <jameskitamirike@gmail.com>
2017-07-03 15:45:34 +0200

    Shoot shot and receive shot work

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,19 +1,23 @@
-import OccupationType from '../models/OccupationType'
+import Vue from 'vue'
 
 export function START_GAME ({ commit }) {
     console.log('game started')
 }
 
-export function SHOOT_SHOT ({ commit }, panel) {
-    // send shot to server and save response
+export function SHOOT_SHOT ({ commit, socket }, panel) {
     let payload = {
         x: panel.Coordinates.x,
-        y: panel.Coordinates.y,
-        occupationType: OccupationType.HIT
+        y: panel.Coordinates.y
     }
+
+    let vinstance = new Vue()
+    vinstance.$socket.emit('FIRE_SHOT', payload)
+}
+
+export function RECEIVE_FIRE_SHOT_RESULT ({ commit }, payload) {
     commit('UPDATE_PANEL', payload)
 }
 
-export function RECEIVE_SHOT ({ commit }) {
-
+export function socket_receiveFireShotResult ({ dispatch }, message) { // eslint-disable-line
+    dispatch('RECEIVE_FIRE_SHOT_RESULT', message)
 }
